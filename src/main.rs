911d4b1321f562f4f465c12e4dfac6bc89583121
Kali Live user <naushnikol@gmail.com>
2024-02-18 10:05:56 +0300

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader, Write};
+use std::hash::Hasher;
+use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::ptr::read;
 use qrcode::QrCode;
 use qrcode::types::QrError;
 use rand::{distributions::Alphanumeric, Rng};
@@ -10,6 +12,7 @@ use teloxide::{
 use image::Luma;
 use teloxide::types::{InputFile, ResponseParameters};
 use tokio::fs;
+use tokio::fs::OpenOptions;
 
 
 #[tokio::main]
@@ -21,12 +24,12 @@ async fn main() {
     Command::repl(bot, answer).await;
 }
 #[derive(BotCommands, Clone)]
-#[command(rename_rule="lowercase", description = "These commands are supported:")]
+#[command(rename_rule = "lowercase", description = "These commands are supported:")]
 enum Command{
     #[command(description="start message handler.")]
     Start,
-    #[command(description="command thant create new qr code.", parse_with = "split")]
-    Create,
+    #[command(description = "handle a username and an age.", parse_with = "split")]
+    Create { activations: String, act: u8},
 }
 
 async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()>{
@@ -40,28 +43,41 @@ async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()>{
             }
             let file = File::open("src/codes.txt")?;
             let reader = BufReader::new(file);
-            let contains = reader.lines().any(|line| {
-                if let Ok(line) = line {
-                    line.contains(msgs[1])
-                } else {
-                    false
+            let mut codes: Vec<(String, u32)> = Vec::new();
+            for line in reader.lines() {
+                let line = line?;
+                let mut parts = line.split_whitespace();
+                if let (Some(code), Some(count_str), None) = (parts.next(), parts.next(), parts.next()) {
+                    if let Ok(count) = count_str.parse::<u32>() {
+                        codes.push((code.to_string(), count));
+                    }
                 }
-            });
-            if contains{
-                bot.send_message(msg.chat.id, "You owned 20 skillcoins.").await?;
-                return Ok(());
             }
-            bot.send_message(msg.chat.id, "This code is not avalible").await?;
+            if let Some(index) = codes.iter().position(|(code, _)| code == msgs[1]) {
+                codes[index].1 -=  1;
+                if codes[index].1 ==  0 {
+                    codes.remove(index);
+                }
+                bot.send_message(msg.chat.id, "You owned 20 skillcoins.").await?;
+            }else{
+                bot.send_message(msg.chat.id, "This code is not avalible").await?;
+            }
+            let file = File::create("src/codes.txt")?;
+            let mut writer = BufWriter::new(file);
+            for (code, count) in codes {
+                writeln!(writer, "{} {}", code, count)?;
+            }
+
         }
-        Command::Create => {
+        Command::Create {activations, act} => {
             let mut file = File::create("src/codes.txt")?;
             let code: String = rand::thread_rng()
                 .sample_iter(&Alphanumeric)
                 .take(5)
                 .map(char::from)
                 .collect();
-            file.write(&format!("{}\n", code).into_bytes())?;
-            let qr = QrCode::new(format!("https://t.me/malik_and_leyla_bot?start={}", code)).unwrap();
+            writeln!(file, "{} {}", code, activations.trim().parse::<u8>().expect("err"))?;
+            let qr = QrCode::new(format!("https://t.me/AppleTeabot?start={}", code)).unwrap();
             let img = qr.render::<Luma<u8>>().build();
             img.save(format!("src/q_{}_code.png", code)).unwrap();
             let qr_send = InputFile::file(format!("src/q_{}_code.png", code));
