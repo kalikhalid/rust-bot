dc5e85f31da283929d8dd7dee74978c9b52d6a68
user <user@192-168-1-150.local>
2024-02-21 16:45:43 +0300

    adding api controller

--- src/api.rs
+++ src/api.rs
@@ -1,42 +1,51 @@
 use std::fs::File;
-use std::io::prelude::*;
-use reqwest::header::HeaderMap;
-use reqwest::Client;
+use std::io::BufReader;
+use std::sync::Arc;
+use anyhow::Ok;
+use reqwest::cookie::Jar;
 use serde_json::Value;
+use reqwest::Client;
+use reqwest::Url;
+use tokio;
+use serde_json;
+use anyhow::Result;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Load headers
-    let mut file = File::open("params/headers.json")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    let headers: HeaderMap = serde_json::from_str(&contents)?;
-
-    // Create a client
-    let client = Client::builder()
-        .default_headers(headers)
+#[tokio::main]
+async fn main() -> Result<()>{
+    let cookie_store: Arc<Jar> = Arc::new(Jar::default());
+    let content = std::fs::read_to_string("params/headers.json").unwrap();
+    let client: Client = Client::builder()
+        .cookie_provider(cookie_store)
+        .cookie_store(true)
         .build()?;
+    let log_file = BufReader::new(File::open("params/log.json")?);
+    let parse_logs: Value  = serde_json::from_reader(log_file)?;
+    let mut params:[(&str, &str); 2] = [
+        ("login", parse_logs["login"].as_str().unwrap()),
+        ("password", parse_logs["password"].as_str().unwrap())
+    ];
 
-    // Load data
-    let mut file = File::open("params/data_sample.json")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    let data: Value = serde_json::from_str(&contents)?;
 
-    // Load params
-    let mut file = File::open("params/log.json")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    let params: Value = serde_json::from_str(&contents)?;
-
-    // Make the POST request
-    let response = client.post("https://skillbox-kazan.docrm.org/api/AccountApi/Login")
-        .json(&params)
-        .send()?;
-
-    // Print the cookies
-    for cookie in response.cookies() {
-        println!("{}", cookie);
+    let url = "https://skillbox-kazan.docrm.org/api/AccountApi/Login";
+    let url: Url = "https://skillbox-kazan.docrm.org/api/AccountApi/Login?login=79279500595&password=123456".parse().unwrap(); //Url::parse_with_params(url, &params)?;
+    println!("{}", url);
+    let res = client.post(url)
+        .form(&params)
+        .body(content)
+        .send()
+        .await?;
+    println!("{}", res.status());
+    let nwe_url = "https://skillbox-kazan.docrm.org".parse::<Url>().unwrap();
+    let headers = res.cookies();
+    for i in headers{
+        println!("{:?}", i);
     }
-
+    println!("{:?}", client);
     Ok(())
 }
+
+pub struct ApiController;
+
+// impl ApiController {
+//     pub fn
+// }
